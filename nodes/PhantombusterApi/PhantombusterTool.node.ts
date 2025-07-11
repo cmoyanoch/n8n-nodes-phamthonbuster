293cb2ce08689ff4934c243d33488da6817a72b0
cmoyanoch <cmoyanoch@gmail.com>
2025-07-10 21:03:37 -0400

    error de ejecucion 3

--- nodes/PhantombusterApi/PhantombusterTool.node.ts
+++ nodes/PhantombusterApi/PhantombusterTool.node.ts
@@ -376,7 +376,7 @@ export class PhantombusterTool implements INodeType {
 								const response = await phantombusterApiRequest.call(
 									executeContext,
 									'GET',
-									'user'
+									'agents'
 								);
 								return JSON.stringify(response, null, 2);
 							} catch (error) {
